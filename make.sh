dc8d1e4c4baf456402dbf226958c5a41820225c4
Evan Nemerson <evan@nemerson.com>
2017-03-15 21:14:57 -0700

    Add Travis CI support.

--- make.sh
+++ make.sh
@@ -1,1 +1,7 @@
-g++ -DNDEBUG -O3 -fomit-frame-pointer -msse2 -std=c++11 -D_FILE_OFFSET_BITS=64 -o mcm Archive.cpp Huffman.cpp MCM.cpp Memory.cpp Util.cpp Compressor.cpp File.cpp LZ.cpp Tests.cpp -lpthread
+#!/bin/sh
+
+if [ ! -n "${CXX}" ]; then
+    CXX=c++
+fi
+
+${CXX} -DNDEBUG -O3 -fomit-frame-pointer -msse2 -std=c++11 -D_FILE_OFFSET_BITS=64 -o mcm Archive.cpp Huffman.cpp MCM.cpp Memory.cpp Util.cpp Compressor.cpp File.cpp LZ.cpp Tests.cpp -lpthread
